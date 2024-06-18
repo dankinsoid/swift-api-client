65c9b6a586c30ecb7d0455ee2a5efae0db472423
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2024-06-18 12:59:08 +0400

    1.27.2

--- Sources/SwiftAPIClient/Extensions/URLComponentBuilder.swift
+++ Sources/SwiftAPIClient/Extensions/URLComponentBuilder.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 /// A protocol that defines a type capable of configuring URLComponents.
 public protocol URLComponentBuilder {
