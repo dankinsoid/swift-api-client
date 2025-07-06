02f82aee76aa0bdab0448fe0ed38510717ef3d72
Danil <voidilov@gmail.com>
2025-07-07 00:09:33 +0300

    fix build

--- Sources/SwiftAPIClient/Utils/AnyAsyncSequence.swift
+++ Sources/SwiftAPIClient/Utils/AnyAsyncSequence.swift
@@ -1,25 +1,24 @@
 import Foundation
 
-#if swift(<6.0)
 /// A type-erasing wrapper for any `AsyncSequence`.
 @available(*, deprecated, message: "This type is deprecated and will be removed in a future version")
 public struct AnyAsyncSequence<Element>: AsyncSequence {
 
 	private var _makeAsyncIterator: () -> AsyncIterator
-
+	
 	/// Initializes a new instance with the provided iterator-making closure.
 	/// - Parameter makeAsyncIterator: A closure that returns an `AsyncIterator`.
 	public init(makeAsyncIterator: @escaping () -> AsyncIterator) {
 		_makeAsyncIterator = makeAsyncIterator
 	}
-
+	
 	/// Initializes a new instance by wrapping an existing `AsyncSequence`.
 	/// - Parameter sequence: An `AsyncSequence` whose elements to iterate over.
 	public init<S: AsyncSequence>(_ sequence: S) where S.Element == Element {
 		self.init {
-			var iterator = sequence.makeAsyncIterator()
+			let box = Box(sequence.makeAsyncIterator())
 			return AsyncIterator {
-				try await iterator.next()
+				try await box.iterator.next()
 			}
 		}
 	}
@@ -28,18 +27,25 @@ public struct AnyAsyncSequence<Element>: AsyncSequence {
 	public func makeAsyncIterator() -> AsyncIterator {
 		_makeAsyncIterator()
 	}
-
+	
 	/// The iterator for `AnyAsyncSequence`.
 	public struct AsyncIterator: AsyncIteratorProtocol {
-
+		
 		private var _next: () async throws -> Element?
-
+		
 		public init(next: @escaping @Sendable () async throws -> Element?) {
 			_next = next
 		}
-
+		
 		public mutating func next() async throws -> Element? {
 			try await _next()
+		}
+	}
+
+	private final class Box<I: AsyncIteratorProtocol> where I.Element == Element {
+		var iterator: I
+		init(_ iterator: I) {
+			self.iterator = iterator
 		}
 	}
 }
@@ -49,8 +55,8 @@ public extension AsyncSequence {
 	/// Erases the type of this sequence and returns an `AnyAsyncSequence` instance.
 	/// - Returns: An instance of `AnyAsyncSequence` wrapping the original sequence.
 	@available(*, deprecated, message: "This method is deprecated and will be removed in a future version")
+	@_disfavoredOverload
 	func eraseToAnyAsyncSequence() -> AnyAsyncSequence<Element> {
 		AnyAsyncSequence(self)
 	}
 }
-#endif
