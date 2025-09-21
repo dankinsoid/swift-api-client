e7f05227ffb28790033520294f630c67fe487e07
Danil <voidilov@gmail.com>
2025-09-21 22:57:26 +0400

    remove async

--- Sources/SwiftAPIClient/Extensions/Async++.swift
+++ Sources/SwiftAPIClient/Extensions/Async++.swift
@@ -1,34 +1,5 @@
 import Foundation
 
-public struct AsyncSequenceOfElements<S: Sequence>: AsyncSequence {
-
-	public typealias AsyncIterator = AsyncStream<S.Element>.AsyncIterator
-	public typealias Element = S.Element
-
-	let sequence: S
-
-	public init(_ sequence: S) {
-		self.sequence = sequence
-	}
-
-	public func makeAsyncIterator() -> AsyncStream<S.Element>.AsyncIterator {
-		AsyncStream { cont in
-			for element in sequence {
-				cont.yield(element)
-			}
-			cont.finish()
-		}
-		.makeAsyncIterator()
-	}
-}
-
-public extension Sequence {
-
-	var async: AsyncSequenceOfElements<Self> {
-		AsyncSequenceOfElements(self)
-	}
-}
-
 func completionToThrowsAsync<T>(
 	_ body: @escaping (CheckedContinuation<T, Error>, CheckedContinuationCancellationHandler) -> Void
 ) async throws -> T {
